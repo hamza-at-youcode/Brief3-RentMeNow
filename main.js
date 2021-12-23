1df78a59b2e2a99409ebfe08d4201b8bdf9b275f
hamza-at-youcode <93930295+hamza-at-youcode@users.noreply.github.com>
2021-12-23 16:37:25 +0100

    Grap input values and alert them

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ menuBtn.addEventListener('click',()=>{
     document.querySelector('.header--nav').classList.toggle('show');
     menuBtn.querySelector('img').setAttribute('src','./assets/icons/icon-close.svg');
 });
+
+document.querySelector('.btn-submit').addEventListener('click',(e)=>{
+    e.preventDefault();
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let msg = document.getElementById("msg").value;
+    alert(`Processing...\nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${msg}`);
+});
