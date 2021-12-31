d1122c2188eda769baed856fa01b74aa76d4e1e3
hamza-at-youcode <93930295+hamza-at-youcode@users.noreply.github.com>
2021-12-31 14:50:47 +0100

    Move script to contact.html

--- main.js
+++ main.js
@@ -3,12 +3,3 @@ menuBtn.addEventListener('click',()=>{
     document.querySelector('.header--nav').classList.toggle('show');
     menuBtn.classList.toggle('arrow');
 });
-
-document.querySelector('.btn-submit').addEventListener('click',(e)=>{
-    e.preventDefault();
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let subject = document.getElementById("subject").value;
-    let msg = document.getElementById("msg").value;
-    alert(`Processing...\nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${msg}`);
-});
