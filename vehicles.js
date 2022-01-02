9caf3e5547cb365e8ac80a64f32cc4cbad26f17f
hamza-at-youcode <93930295+hamza-at-youcode@users.noreply.github.com>
2022-01-02 21:18:18 +0100

    Prepare vehicles data

---
+++ vehicles.js
@@ -0,0 +1,46 @@
+const vehicles = [
+    {
+        type:"Moto",
+        price: 10,
+        gearbox:false,
+        fuel:["Electrique","Essence"]
+    },
+    {
+        type:"Citadine",
+        price: 12,
+        gearbox:"Manuelle",
+        fuel:["Electrique","Hybride","Essence","Diesel"]
+    },
+    {
+        type:"Compact",
+        price: 14,
+        gearbox:"Manuelle",
+        fuel:["Hybride","Essence","Diesel"]
+    },
+    {
+        type:"Berline",
+        price: 20,
+        gearbox:"Automatique",
+        fuel:["Hybride","Essence","Diesel"]
+    },
+    {
+        type:"Utilitaire",
+        price: 16,
+        gearbox:"Manuelle",
+        fuel:["Diesel"]
+    },
+    {
+        type:"Engin de Chantier",
+        price: 900,
+        gearbox:"Manuelle",
+        fuel:["Essence","Diesel"]
+    },
+    {
+        type:"Camion",
+        price: 250,
+        gearbox:"Automatique",
+        fuel:["Diesel"]
+    }
+];
+
+export default vehicles;
